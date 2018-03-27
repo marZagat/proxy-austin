d7956c167434fd217db60216f48e0eacea97bb95
Sr5x4FC3s <32333361+Sr5x4FC3s@users.noreply.github.com>
2018-03-26 18:57:01 -0700

    implemented server side rendering

--- server.js
+++ server.js
@@ -7,13 +7,13 @@ const port = process.env.PORT || 3000;
 const request = require('request');
 
 app.use(morgan('dev'));
-// app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')));
 
 // app.get('/', (req, res) => {
 //   res.redirect('/restaurants/ChIJUcXYWWGAhYARmjMY2bJAG2s/');
 // });
 
-app.use('/restaurants/:id', express.static(path.join(__dirname, 'public')));
+// app.use('*', express.static(path.join(__dirname, 'public')));
 
 // app.get('/api/restaurants/:id/gallery', (req, res) => {
 //   res.redirect(`http://13.57.148.57/api/restaurants/${req.params.id}/gallery`)
@@ -30,6 +30,35 @@ app.get('/api/restaurants/:id/gallery', (req, res) => {
 // app.get('/api/restaurants/:id/recommendations', (req, res) => {
 //   res.redirect(`http://52.89.102.101/api/restaurants/${req.params.id}/recommendations`)
 // });
+
+const clientBundles = './public/services';
+const serverBundles = './templates/services';
+const serviceConfig = require('./service-config.json');
+const services = require('./loader.js')(clientBundles, serverBundles, serviceConfig);
+
+const React = require('react');
+const ReactDom = require('react-dom/server');
+const Layout = require('./templates/layouts');
+const App = require('./templates/app');
+const Scripts = require('./templates/scripts');
+
+// see: https://medium.com/styled-components/the-simple-guide-to-server-side-rendering-react-with-styled-components-d31c6b2b8fbf
+const renderComponents = (components, props = {}) => {
+  return Object.keys(components).map(item => {
+    let component = React.createElement(components[item], props);
+    return ReactDom.renderToString(component);
+  });
+}
+
+app.get('/restaurants/:id/', function(req, res){
+  let components = renderComponents(services, {itemid: req.params.id});
+  console.log(services);
+  res.end(Layout(
+    'marZagat',
+    App(...components),
+    Scripts(Object.keys(services))
+  ));
+});
 
 app.listen(port, () => {
   console.log(`server running at: http://localhost:${port}`)
---
+++ templates/app.js
@@ -0,0 +1,6 @@
+module.exports = (photos, countdown, related, reviews) => `
+<div id="App">${photos}</div>
+<div id="CountDown">${countdown}</div>
+<div id="Related">${related}</div>
+<div id="Reviews">${reviews}</div>
+`;
